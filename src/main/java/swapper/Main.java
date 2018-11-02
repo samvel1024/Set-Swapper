016efadaa59dc421bc374bbc2b4a46bda970dacc
Samvel Abrahamyan <samvel1024@gmail.com>
2018-11-02 23:00:45 +0100

    Init

---
+++ src/main/java/swapper/Main.java
@@ -0,0 +1,7 @@
+package swapper;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Bik");
+	}
+}
---
+++ src/main/java/swapper/Swapper.java
@@ -0,0 +1,16 @@
+package swapper;
+
+import java.util.Collection;
+
+public class Swapper<T>{
+
+	public Swapper(){
+
+	}
+
+
+	public void swap(Collection<T> rem, Collection<T> add){
+
+	}
+
+}
